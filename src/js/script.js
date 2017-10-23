9b2430de91d2cacabf62608befcb0c77cd464364
Joshua Gangadeen <joshuaxyoshi@yahoo.ca>
2017-10-23 17:05:33 -0400

    got articles

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@ console.log("hi");
 
 var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
 var allResult = {};
-var thumb = {};
+var thumb = "";
 url += '?' + $.param({
   'api-key': "871145aeefe14e919fe0c8cf62533730"
 });
@@ -12,12 +12,24 @@ $.ajax({
   method: 'GET',
 }).done(function(result) {
   allResult = result;
-  thumb = allResult.results["0"].multimedia[4].url;
+
+  for (var i=0;i<=23;i++){
+  if(allResult.results[i].multimedia.length == 0){
+    $('div#articles').append('<a class="article" style=background-image:url("../assets/images/image-not-found.png");></a>');
+    $('a.article').last().append('<p>' + allResult.results[i].abstract + '</p>');
+    $('a.article').last().attr("href",allResult.results[i].url);
+  }else{
+    thumb = allResult.results[i].multimedia[3].url;
+    $('div#articles').append('<a class="article" style=background-image:url(' + thumb + ');'  +  ' /></a>');
+    $('a.article').last().append('<p>' + allResult.results[i].abstract + '</p>');
+    $('a.article').last().attr({'href':allResult.results[i].url});
+  };
+};
+  if($(select#sections.world) == "world"){
+    allResult.section = "world"
+  };
+
 }).fail(function(err) {
   throw err;
 });
 console.log(allResult);
-//for (var i=0;i<12;i++){console.log(allResult.results[i])}
-$(document).ready(function(){
-$('div#articles').append('<img src=' + thumb + ' ' + '/>')
-});
