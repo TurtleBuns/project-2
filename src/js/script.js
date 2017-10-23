583052406215640a2f824ecb4df787d2f1089533
TurtleBuns <32001420+TurtleBuns@users.noreply.github.com>
2017-10-22 22:24:18 -0400

    add thumb

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@ console.log("hi");
 
 var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
 var allResult = {};
+var thumb = {};
 url += '?' + $.param({
   'api-key': "871145aeefe14e919fe0c8cf62533730"
 });
@@ -11,10 +12,12 @@ $.ajax({
   method: 'GET',
 }).done(function(result) {
   allResult = result;
+  thumb = allResult.results["0"].multimedia[4].url;
 }).fail(function(err) {
   throw err;
 });
 console.log(allResult);
 //for (var i=0;i<12;i++){console.log(allResult.results[i])}
-var thumb = allResult.results["0"].multimedia[3].url
-$('section#articles').append('<img src=' + allResult.results["0"].multimedia[3].url + '/>')
+$(document).ready(function(){
+$('div#articles').append('<img src=' + thumb + ' ' + '/>')
+});
